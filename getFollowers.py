013ba16a3bded2dd55a325487df517e3b4136f09
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2019-07-02 14:56:04 -0400

    Basic functionality
    Add feature to get a list of a user's followers given their user ID

---
+++ getFollowers.py
@@ -0,0 +1,32 @@
+import json
+import os.path
+from Login import api
+
+try:
+    from instagram_private_api import (
+        Client, __version__ as client_version)
+
+except ImportError:
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    from instagram_private_api import (
+        Client, __version__ as client_version)
+
+# ---------- Pagination with max_id ----------
+user_id = input("Enter a user ID: ")
+followers = []
+rank_token = Client.generate_uuid()
+results = api.user_followers(user_id, rank_token)
+followers.extend(results.get('users', []))
+
+next_max_id = results.get('next_max_id')
+while next_max_id:
+    results = api.user_followers(user_id, rank_token, max_id=next_max_id)
+    followers.extend(results.get('users', []))
+    if len(followers) >= 600:    # get only first 600 or so
+        break
+    next_max_id = results.get('next_max_id')
+
+followers.sort(key=lambda x: x['pk'])
+# print list of user IDs
+print(json.dumps([u['pk'] for u in followers], indent=2))
