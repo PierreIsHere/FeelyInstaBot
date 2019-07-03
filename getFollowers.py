dbaca8c34875ec2696232a65481040d707d09e38
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-07-03 09:35:26 -0400

    Begin stages of tag search

--- getFollowers.py
+++ getFollowers.py
@@ -30,3 +30,15 @@ while next_max_id:
 followers.sort(key=lambda x: x['pk'])
 # print list of user IDs
 print(json.dumps([u['pk'] for u in followers], indent=2))
+
+feed = api.feed_tag("pianoschool",rank_token)
+posts = feed['ranked_items']
+
+for i in range(len(posts)-1): 
+    print(posts[i]["id"])
+print
+feed = api.feed_tag("pianoschool",rank_token)
+posts = feed['ranked_items']
+
+for i in range(len(posts)-1): 
+    print(posts[i]["id"])
