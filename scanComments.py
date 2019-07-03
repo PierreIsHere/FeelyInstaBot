e61940eb1bc31d86ca14e10c043ac176ba4fbc07
Wilson Sy <sy.wilson@protonmail.com>
2019-07-03 17:03:56 -0400

    Add profanity detection
    Auto-deletes comments if they contain words found in profanity.txt

--- scanComments.py
+++ scanComments.py
@@ -10,5 +10,24 @@ except ImportError:
     from instagram_private_api import (
         Client, __version__ as client_version)
 
+f = open("profanity.txt", "r")
+profanity_list = [l[:-1] for l in f.readlines()]
+f.close()
+
 def scan(id):
+    comments = api.media_n_comments(id, n=100, reverse=False)
+    delete_queue = []
+    try:
+        for c in comments:
+            for word in c['text'].split():
+                if word.lower() in profanity_list:
+                    print("Detected profanity in comment", c['pk'])
+                    delete_queue.append(c['pk'])
+                    break
+        if len(delete_queue):
+            api.bulk_delete_comments(id, delete_queue)
+            delete_queue.clear()
+    except IndexError:
+        print("<no comments found>")
+
     print("Scanned post", id)
