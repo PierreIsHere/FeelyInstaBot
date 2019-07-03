b393129d2d812c2849a81ad57a64a39f45a978ab
Wilson Sy <sy.wilson@protonmail.com>
2019-07-03 17:01:23 -0400

    Add ability to detect new post event

--- autoModerate.py
+++ autoModerate.py
@@ -13,8 +13,11 @@ except ImportError:
         Client, __version__ as client_version)
 
 target_user_id = '4155012351'
-target_user_feed = api.user_feed(target_user_id)    # returns dictionary of posts
+target_user_feed = api.user_feed(target_user_id)
 guarded_posts = {}
+
+# store number of posts
+p_count = api.user_info(target_user_id)['user']['media_count']
 
 # store comment counts for all posts
 # scans all comments
@@ -22,9 +25,15 @@ for post_id, c_count in [(p['pk'], p['comment_count']) for p in target_user_feed
     scan(post_id)
     guarded_posts[post_id] = c_count
 
-# continuously monitor stored posts for change in comment count
+# continuously monitor user feed
 while True:
     has_changed = False
+    # detects increase in post count
+    if api.user_info(target_user_id)['user']['media_count'] > p_count:
+        target_user_feed = api.user_feed(target_user_id)
+        scan(target_user_feed['items'][0]['pk'])
+
+    # detects change in comment count per post
     for guarded_post_id in guarded_posts.keys():
         current_c_count = api.media_info(guarded_post_id)['items'][0]['comment_count']
         if current_c_count != guarded_posts[guarded_post_id]:
